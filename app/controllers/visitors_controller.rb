7c745009b30b30faf128636b3abc5b1914467886
kellytw <kellytw@earthlink.net>
2014-07-05 18:07:07 -0400

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,19 @@
 class VisitorsController < ApplicationController
-
-	def new
-		#use either the new template or point to a file.  Only one of the 3 lines of code should be executed to show the view
-		@owner = Owner.new #looks for the new template
-		#render 'visitors/new' #looks for the file by defaul the layout will be added
-		#render 'visitors/new', layout: false #this will not grab the layout
-		#flash[:notice] = "Welcome!"
-		#	if @owner.countdown < 365
-		#	flash[:alert] = "Remember my birthday is coming up"
-	end
+def new
+@visitor = Visitor.new
+end
+def create
+@visitor = Visitor.new(secure_params)
+if @visitor.valid?
+@visitor.subscribe
+flash[:notice] = "Signed up #{@visitor.email}."
+redirect_to root_path
+else
+render :new
+end
+end
+private
+def secure_params
+params.require(:visitor).permit(:email)
+end
 end
